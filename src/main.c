ec018309c2959d71e16fa35a3e949c463c37c42f
Charly Poirier <pro.poiriercharly@gmail.com>
2021-03-13 00:05:10 +0100

    Cleaner comments for each step

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 
 /**
  * See https://en.wikipedia.org/wiki/SHA-2
+ * and https://github.com/amosnier/sha-2/blob/master/sha-256.c
  */
 int main(int argc, char** argv) {
     
@@ -14,16 +15,20 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    char* message = argv[1];
-    size_t length = strlen(message) * 8;
+    char* input = argv[1];
+    size_t len = strlen(input) * 8;
+
+    // Pre-processing
+    
+    // Split in 512-bit blocks
 
     // Hash values
     uint32_t h[] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
         0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
 
-    ///////////
-    // To do //
-    ///////////
+    // Message schedule (w0..w15 binary operations)
+    
+    // Compression (h0..h15 binary operations)
 
     // Final digest
     for (int i=0; i<8; ++i) {
