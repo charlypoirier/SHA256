92d763211f7537c8d791d82115e36e2f431beaf2
Charly Poirier <pro.poiriercharly@gmail.com>
2021-03-15 23:02:43 +0100

    Started making blocks and padding

--- src/main.c
+++ src/main.c
@@ -16,42 +16,45 @@ int main(int argc, char** argv) {
 
     // Input data
     uint8_t* data = (uint8_t*)argv[1];
-    uint64_t length = strlen(argv[1]);
-    uint64_t bits = length * 8;
-    
-    printf("Input data (decimal)\n");
-    for (int i=0; i<length; ++i) {
-        printf("%d ", data[i]);
-    }
-    printf("\n");
-
-    // Buffer
-    uint8_t buffer[64];
-    uint8_t size = length;
-    memset(buffer, 0, sizeof(buffer));
-    
-    printf("\nBuffer (hexadecimal)\n");
-    for (int i=0; i<64; ++i) {
-        printf("%02x ", buffer[i]);
-    }
-    printf("\n");
+    uint64_t size = strlen(argv[1]);
+    uint64_t totalSize = size;
 
     // Initial hash values
     uint32_t h[] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
         0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
 
-    // Pre-processing
-    memcpy(buffer, data, size);
-    buffer[size++] = 0b10000000;
+    // Process 512-bit chunks
+    uint8_t chunk[64];
+    memset(chunk, 0, sizeof(chunk));
 
-    // Split in 512-bit blocks
+    while (size > 0) {
 
-    // Message schedule (w0..w15 binary operations)
-    
-    // Compression (h0..h15 binary operations)
+        int n = size < 64 ? size : 64;
+        memcpy(chunk, data, n);
+        data = data + n;
+
+        // Padding
+        if (64 - n > 0) {
+            chunk[n] = 0b10000000;
+            for (int i=n+1; i<64; ++i) {
+                chunk[i] = 0b00000000;
+            }
+        }
+
+        printf("\n");
+        for (int i=0; i<64; ++i) {
+            printf("%02x ", chunk[i]);
+        }
+        printf("\n");
+
+        // Message schedule (w0..w15 binary operations)
+        // Compression (h0..h15 binary operations)
+
+        size -= n;
+    }
 
     // Final hash
-    printf("\nFinal hash\n");
+    printf("\n");
     for (int i=0; i<8; ++i) {
         printf("%08x", h[i]);
 	}
