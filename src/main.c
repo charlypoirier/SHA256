1577dc69a1ecaef10ba7ef67b592231d3bed0d51
Charly Poirier <pro.poiriercharly@gmail.com>
2021-03-18 21:33:19 +0100

    Message schedule and compression

--- src/main.c
+++ src/main.c
@@ -19,15 +19,17 @@ int main(int argc, char** argv) {
     uint64_t size = strlen(argv[1]);
     uint64_t totalSize = size;
 
-    // Initial hash values
+    // Current hash values
     uint32_t h[] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
         0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
+
+    // Message schedule
+    uint32_t W[64];
 
     // Process 512-bit chunks
     uint8_t chunk[64];
     memset(chunk, 0, sizeof(chunk));
-    
-    short done = 0;
+    int done = 0;
 
     while (!done) {
 
@@ -52,21 +54,34 @@ int main(int argc, char** argv) {
             }
         }
 
-        printf("\n");
-        for (int i=0; i<64; ++i) {
-            printf("%02x ", chunk[i]);
+        // Message schedule
+        for (int i=0, j=0; i<16; ++i, j+=4) {
+            W[i] = (chunk[j] << 24) | (chunk[j+1] << 16) | (chunk[j+2] << 8) | (chunk[j+3]);
         }
-        printf("\n");
 
-        // Message schedule (w0..w15 binary operations)
-        // Compression (h0..h15 binary operations)
+        for (int i=16; i<64; ++i) {
+            W[i] = s1(W[i-2]) + W[i-7] + s0(W[i-15]) + W[i-16];
+        }
+
+        // Compression
+        for (int i=0; i<64; ++i) {
+            uint32_t T1 = S1(h[4]) + choice(h[4], h[5], h[6]) + h[7] + K[i] + W[i];
+            uint32_t T2 = S0(h[0]) + majority(h[0], h[1], h[2]);
+            for (int j=7; j>0; --j) {
+                h[j] = h[j-1];
+            }
+            h[0] = T1 + T2;
+            h[4] += T1;
+        }
+
+        for (int i=0; i<7; ++i) {
+            h[i] += H[i];
+        }
 
         size -= n;
     }
 
-
     // Final hash
-    printf("\n");
     for (int i=0; i<8; ++i) {
         printf("%08x", h[i]);
 	}
